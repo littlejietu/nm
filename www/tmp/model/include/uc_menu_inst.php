1a5b41389760f7ffbc47f171e60729547dcbfd9c
littljietu <littljietu@qq.com>
2015-06-05 18:44:30 +0800

    ~

--- www/tmp/model/include/uc_menu_inst.php
+++ www/tmp/model/include/uc_menu_inst.php
@@ -9,17 +9,19 @@
     </div>
     <div class="menu_box">
         <ul>
+            <!--<li><a href="inst_broker.php">经纪人管理<i></i></a></li>-->
         	<li><a href="inst_artistmanag.php">艺人管理<i></i></a></li>
             <li><a href="inst_interactive.php">互动总览<i></i></a></li>
             <li class="current"><a href="inst_profile.php">机构资料<i></i></a></li>
-            <li><a href="inst_price.php">服务价格<i></i></a></li>
+            <!--<li><a href="inst_price.php">服务价格<i></i></a></li>-->
             <li><a href="inst_authent.php">机构认证<i></i></a></li>
             <li><a href="inst_mworks.php">作品管理<i></i></a></li>
-            <li><a href="inst_order.php">交易管理<span class="o_mete">8</span><i></i></a></li>
-            <li><a href="##">资金账户<i></i></a></li>
+            <!--<li><a href="inst_order.php">交易管理<span class="o_mete">8</span><i></i></a></li>
+            <li><a href="##">资金账户<i></i></a></li>-->
             <li><a href="inst_review.php">评论管理<i></i></a></li>
             <li><a href="inst_client.php">客户管理<i></i></a></li>
             <li><a href="inst_letter.php">站内信<i></i></a></li>
+            <li><a href="inst_notice.php">机构通告<i></i></a></li>
         </ul>
     </div>
     <div class="m_level">
