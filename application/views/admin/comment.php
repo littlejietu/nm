fd8cf98a4d9ba6bcad2cc4c6f205aae8332e49ab
littlejietu <littlejietu@qq.com>
2015-06-22 12:55:21 +0800

    i

--- application/views/admin/comment.php
+++ application/views/admin/comment.php
@@ -54,11 +54,9 @@
 	<table cellpadding="0" cellspacing="0" align="center" bordercolor="#eee" border="1" width="100%" class="listTab">
 		<tbody>
 			<tr height="39" style="font-size:13px;">
-	            <td width="250" height="25" align="center">被评价用户</td>
 	            <td>被评价昵称</td>
 	            <td>评价人用户</td>
 	            <td>评价人昵称</td>
-	            <td>评价人头像</td>
 	            <td>回复</td>
 	            <td>身材样貌</td>
 	            <td>专业技能</td>
@@ -74,11 +72,9 @@
 	        </tr>
 	        <?php foreach ($list['rows'] as $key => $a): ?>
 			<tr>
-				<td height="30"><?php echo $a['touserid'];?></td>
-				<td><?php echo $a['tonickname'];?></td>
+				<td height="30"><?php echo $a['tonickname'];?></td>
 				<td><?php echo $a['userid'];?></td>
 				<td><?php echo $a['nickname'];?></td>
-				<td><?php echo $a['logo'];?></td>
 				<td><?php echo $a['commentid'];?></td>
 				<td><?php echo $a['figure'];?></td>
 				<td><?php echo $a['skill'];?></td>
@@ -88,7 +84,7 @@
 				<td><?php echo $a['good'];?></td>
 				<td><?php echo date('Y-m-d H:i:s',$a['addtime']);?></td>
 				<td><?php if($a['display']==1) echo '显示'; else if($a['display']==2) echo '不显示';  else echo '';?></td>
-				<td><?php echo $a['op_username'];?></td>
+				<td><?php echo $a['op_nickname'];?></td>
 				<td><?php echo date('Y-m-d H:i:s',$a['op_time']);?></td>
 				<td class="con_title">
 					<a href="/admin/comment/add?id=<?php echo _get_key_val($a['id']);?>">修改</a>
